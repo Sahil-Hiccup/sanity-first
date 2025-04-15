52ce987bcb95b93e7ebc80a338227c4f8dc5bf3c
Sahil Singh <sahil@hiccupsolutions.com>
2025-04-15 15:41:32 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -63,7 +63,7 @@ const HomePage = async () => {
         <div className="container mx-auto">
           <div className="text-center mb-10">
             <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">
-              Latest From The Blog
+              Latest From The Blogs
             </h1>
             <div className="flex mt-6 justify-center">
               <div className="w-16 h-1 rounded-full bg-indigo-500 inline-flex" />
